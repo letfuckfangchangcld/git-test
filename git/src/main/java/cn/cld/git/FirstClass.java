12a894280572448db08ecb31b44e39cb3481d02f
cld <877566926@qq.com>
2019-11-07 14:07:06 +0800

    把本地工程添加到git

---
+++ git/src/main/java/cn/cld/git/FirstClass.java
@@ -0,0 +1,9 @@
+package cn.cld.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("你好，git");
+        System.out.println("hello.git");
+        System.out.println("阿牛撒有,git");
+    }
+}
